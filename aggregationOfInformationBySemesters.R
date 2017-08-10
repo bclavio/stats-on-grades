ef9dff361a246c9e25a457cde128d2d857496378
Hendrik Knoche <hendrik.knoche@gmail.com>
2017-08-10 11:47:23 +0200

    hell again

--- aggregationOfInformationBySemesters.R
+++ aggregationOfInformationBySemesters.R
@@ -23,7 +23,7 @@ library(sandwich)
 ## some testing
 ################## COMMENT AGAIN
 ##hk comment
-#hllo
+#hell
 
 dfECTSstruct<-gsheet2tbl('https://docs.google.com/spreadsheets/d/10xp3CLhDkgCG2p3M2f8GrGQ4j5kNaqrdJg1cFBAb7DQ/edit?usp=sharing')
 #used to be  <-read.csv("course_SPV.csv", header = TRUE, fill=TRUE, sep = ",",fileEncoding = "UTF-8")
