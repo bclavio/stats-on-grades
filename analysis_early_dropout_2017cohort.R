e8641c99b7077da11c12efaefd758a037db66b0f
nvihrs14 <nvihrs14@student.aau.dk>
2018-03-15 08:20:51 +0100

    adding manual values from SSP questions

--- analysis_early_dropout_2017cohort.R
+++ analysis_early_dropout_2017cohort.R
@@ -2,76 +2,107 @@ library(tidyr)
 library(glmnet)
 library(rpart)
 library(rpart.plot)
+
+#Loading data from fileshare
 dropoutFeb18 <- read.csv("Y:/analysis_data/dropOut/data/dropout-Feb2018-Q999.csv", encoding="UTF-8",stringsAsFactors=FALSE)
 names(dropoutFeb18)[1] <- 'Name'
 
-questions<-read.csv("C:/Users/Helle/Downloads/2018_SLERD_Paper_Analysis/Paper/QuestionsOverview.csv", header = TRUE, fill=TRUE, sep = ",", check.names=FALSE, stringsAsFactors=FALSE)
-SSP<-read.csv("Y:/analysis_data/SSP/SSPgradesTestAAL 02-10.csv", header = TRUE, fill=TRUE, sep = ",", check.names=FALSE, stringsAsFactors=FALSE, encoding="UTF-8")
-names(SSP)[1] <- 'Surname'
-names(SSP)[5] <- 'Email'
-SSPName <- unite_(SSP,'Name', c("First name","Surname"), sep = ' ')
-SSPName <- SSPName[-92,]
+questions<-read.csv("Y:/analysis_data/SSP/QuestionsOverview.csv", header = TRUE, fill=TRUE, sep = ",", check.names=FALSE, stringsAsFactors=FALSE)
+SSPAAL<-read.csv("Y:/analysis_data/SSP/SSPgradesTestAAL 02-10.csv", header = TRUE, fill=TRUE, sep = ",", check.names=FALSE, stringsAsFactors=FALSE, encoding="UTF-8")
+SSPCPH <- read.csv("Y:/analysis_data/SSP/SSPgradesTestCPH 02-10.csv", header = TRUE, fill=TRUE, sep = ",", check.names=FALSE, stringsAsFactors=FALSE, encoding="UTF-8")
+SSPAAL$Campus <- 'AAL'
+SSPCPH$Campus <- 'CPH'
+SSPAAL <- SSPAAL[-nrow(SSPAAL),]
+SSPCPH <- SSPCPH[-nrow(SSPCPH),]
+names(SSPAAL)[1] <- 'Surname'
+SSPAAL <- unite_(SSPAAL,'Name', c("First name","Surname"), sep = ' ')
+names(SSPCPH)[1] <- 'Surname'
+SSPCPH <- unite_(SSPCPH,'Name', c("First name","Surname"), sep = ' ')
 
-SSPDropout <- merge(dropoutFeb18,SSPName, by='Name', all.x = TRUE, all.y = TRUE)
-na <- (SSPDropout$Email.x==''|is.na(SSPDropout$Email.x))
-SSPDropout$Email.x[na] <-SSPDropout$Email.y[na]
+#Filling in manual answers
+SSPManualAAL <-read.csv("Y:/analysis_data/SSP/SSPanswersTestAAL 10-10.csv", header = TRUE, fill=TRUE, sep = ",", check.names=FALSE, stringsAsFactors=FALSE, encoding="ANSI")
+SSPManualAAL <- unite_(SSPManualAAL,'Name', c("First name","Surname"), sep = ' ')
+which(!SSPAAL$Name%in%SSPManualAAL$Name)
+#Removing the one person who is in progress
+SSPAAL <- SSPAAL[-91,]
+#Check that all observation coinside and insert the values
+all.equal(SSPAAL$Name,SSPManualAAL$Name)
+SSPAAL[,c('Q. 93 /0.09','Q. 95 /0.09','Q. 96 /0.09')] <- SSPManualAAL[,c('Response 93','Response 95','Response 96')]
+
+SSPManualCPH <-read.csv("Y:/analysis_data/SSP/SSPanswersTestCPH 10-10.csv", header = TRUE, fill=TRUE, sep = ",", check.names=FALSE, stringsAsFactors=FALSE, encoding="ANSI")
+SSPManualCPH <- unite_(SSPManualCPH,'Name', c("First name","Surname"), sep = ' ')
+#Check that all observation coinside and insert the values
+all.equal(SSPCPH$Name,SSPManualCPH$Name)
+SSPCPH[,c('Q. 93 /0.09','Q. 95 /0.09','Q. 96 /0.09')] <- SSPManualCPH[,c('Response 93','Response 95','Response 96')]
+
+SSP <- rbind(SSPAAL,SSPCPH)
+names(SSP)[4] <- 'Email'
+#Renaming questions
+Q <- rep('Q',111)
+num <- 1:111
+dat <- data.frame(Q,num)
+dat <- unite_(dat,'names', c("Q","num"), sep = '')
+names(SSP)[9] <- 'Grade'
+names(SSP)[10:120] <- dat[,1]
+
+SSP$Name[which(!SSP$Name%in%dropoutFeb18$Name)]
+#There seems to be one person who have taken SSP but are not in dropout data
+dropoutFeb18$Name[which(!dropoutFeb18$Name%in%SSP$Name)]
+# There seems to be three people in dropout data who have not taken SSP
+
+#Merge dropout and SSP
+SSPDropout <- merge(dropoutFeb18,SSP, by='Name', all.x = TRUE)
+SSPDropout[SSPDropout==''] <- NA
+SSPDropout[SSPDropout=='-'] <- NA
+SSPDropout$Email.x[is.na(SSPDropout$Email.x)] <-SSPDropout$Email.y[is.na(SSPDropout$Email.x)]
 SSPDropout <- SSPDropout[,-(6:8)]
-SSPDropout$Campus[SSPDropout$Campus==''] <- NA
-#SSP is only taken i AAL so students with answers must be from AAL
-SSPDropout$Campus[!is.na(SSPDropout$State)&is.na(SSPDropout$Campus)] <- 'AAL'
-#Assuming the others with missing campus are from CPH
-SSPDropout$Campus[is.na(SSPDropout$Campus)] <- 'CPH'
+SSPDropout$Campus.x[is.na(SSPDropout$Campus.x)] <- SSPDropout$Campus.y[is.na(SSPDropout$Campus.x)]
+SSPDropout <- SSPDropout[,-122]
+names(SSPDropout)[4] <- 'Campus'
 table(SSPDropout$Status,SSPDropout$Campus)
 
+#In the analysis Q999 is treated as dropout
+SSPDropout$Status[SSPDropout$Status=='Q999'] <- 'dropout'
+#Excluding unnecesarry columns and formatting data
+SSPDropout$Status <- factor(SSPDropout$Status, levels = c('dropout','active'))
+SSPDropout <- SSPDropout[,-c(1,2,5,7:9)]
+SSPDropout[,4:115] <- apply(SSPDropout[,4:115],MARGIN = 2,as.numeric)
+
+#Making another dataset with questions sorted into categories
+SSP <- SSPDropout[!is.na(SSPDropout$State),]
+Qnolab <- questions[,c('Category','QuestionLabel','QuestionNo')]
+Qnolab <- Qnolab[!duplicated(Qnolab),]
+lab <- Qnolab$QuestionLabel[Qnolab$QuestionNo]
+names(SSP)[5:115] <- lab
+cat <- factor(questions$Category)
+
+for (i in 1:length(levels(cat))){
+  category <- levels(cat)[i]
+  col <- names(SSP) %in% Qnolab$QuestionLabel[Qnolab$Category==category]
+  SSP[,115+i] <- rowMeans(SSP[,col], na.rm = TRUE)
+}
+names(SSP)[116:129] <- levels(cat)
+
+SSPQuestions <- SSP[,c(1,5:115)]
+names(SSPQuestions)[2:112] <- names(SSPDropout)[5:115]
+SSPQuestions <- SSPQuestions[,-ncol(SSPQuestions)]
+#Remove last question (comments) because it is always the same
+SSPCategory <- SSP[,c(1,116:129)]
+which(is.na(SSPQuestions))
+###NEED TO REPLACE MISSING VALUES
+
+######################################
+#########Analysis#####################
+######################################
+
 #Checking whether campus has any influence on dropout
-#OBS! in the following Q999 is treated as dropout
-drop <- factor(SSPDropout$Status)
-drop[drop=='Q999'] <- 'dropout'
-drop <- factor(drop)
+
 logCampus <- glm(drop~Campus, family = binomial(link='logit'), data = SSPDropout)
 summary(logCampus)
 drop1(logCampus, test = 'Chisq')
 #Does not seem to have any influence
 
 ####Shifting focus to SSP questions
-Qnolab <- questions[,c('Category','QuestionLabel','QuestionNo')]
-Qnolab <- Qnolab[!duplicated(Qnolab),]
-lab <- Qnolab$QuestionLabel[Qnolab$QuestionNo]
-#Making the names easier to interpret in the analysis
-names(SSPDropout)[11:121] <- lab
-SSP <- SSPDropout[SSPDropout$State=='Finished'&!is.na(SSPDropout$State),]
-SSP[SSP=='-'] <- NA
-SSP[,11:121] <- apply(SSP[,11:121],MARGIN = 2,as.numeric)
-#Sorting into categories
-cat <- factor(questions$Category)
-
-for (i in 1:length(levels(cat))){
-  category <- levels(cat)[i]
-  col <- names(SSP) %in% Qnolab$QuestionLabel[Qnolab$Category==category]
-  SSP[,121+i] <- rowMeans(SSP[,col], na.rm = TRUE)
-}
-names(SSP)[122:135] <- levels(cat)
-
-
-for (i in 1:length(levels(cat))){
-  category <- levels(cat)[i]
-  col <- names(SSP) %in% Qnolab$QuestionLabel[Qnolab$Category==category]
-  SSP[,135+i] <- rowSums(SSP[,col], na.rm = TRUE)
-}
-SSP$`Grade/10.00` <- as.numeric(SSP$`Grade/10.00`)
-names(SSP)[10] <- 'SSPGrade'
-
-SSPQuestions <- SSP[,c(3,11:121)]
-SSPQuestions$Status[SSPQuestions$Status=='Q999'] <-'dropout' 
-SSPQuestions$Status <- factor(SSPQuestions$Status, levels = c('dropout','active'))
-SSPCategoryAVG <- SSP[,c(3,122:135)]
-SSPCategoryAVG$Status[SSPCategoryAVG$Status=='Q999'] <-'dropout' 
-SSPCategoryAVG$Status <- factor(SSPCategoryAVG$Status, levels = c('dropout','active'))
-SSPCategorySum <- SSP[,c(3,136:149)]
-names(SSPCategorySum)[2:15] <- levels(cat)
-SSPCategorySum$Status[SSPCategorySum$Status=='Q999'] <-'dropout' 
-SSPCategorySum$Status <- factor(SSPCategorySum$Status, levels = c('dropout','active'))
-
 
 #Making some different logistic regression models
 loggrade <- glm(SSPQuestions$Status~SSP$SSPGrade, family = binomial(link = 'logit'))
@@ -162,82 +193,6 @@ drop[maxp,]
 summary(logcat)
 #Does not really indicate that any categories are significant
 
-###Tries to repeat it with sum instead of average
-
-logfullcatsum <- glm(Status~., data = SSPCategorySum, family=binomial(link = 'logit'))
-summary(logfullcatsum)
-drop <- drop1(logfullcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-as.data.frame(drop)[maxp,]
-logcatsum <- update(logfullcatsum,~.-`Education Choice Factors`)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-logcatsum <- update(logcatsum,~.-`Attitude Towards Education`)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-logcatsum <- update(logcatsum,~.-Demographics)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-logcatsum <- update(logcatsum,~.-`Self-control`)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-logcatsum <- update(logcatsum,~.-Grit)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-logcatsum <- update(logcatsum,~.-`Growth Mindset`)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-logcatsum <- update(logcatsum,~.-`View on Medialogy`)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-logcatsum <- update(logcatsum,~.-`Reasons for Going to University`)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-logcatsum <- update(logcatsum,~.-`Personal Trait Comparison`)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-logcatsum <- update(logcatsum,~.-`High School Trust`)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-logcatsum <- update(logcatsum,~.-`Perceived Academic Abilities`)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-logcatsum <- update(logcatsum,~.-`Studying and Working Hours`)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-logcatsum <- update(logcatsum,~.-`Belonging Uncertainty`)
-
-drop <- drop1(logcatsum, test = 'Chisq')
-maxp <- which.max(drop$`Pr(>Chi)`)
-drop[maxp,]
-
-summary(logcatsum)
-#Same conclusion
-
 #Try to chose the model with AIC and BIC
 catAIC <- step(logfullcat)
 catBIC <- step(logfullcat, k=log(nrow(SSPCategoryAVG)))
@@ -250,21 +205,6 @@ for (i in 1:length(q)){
 }
 #There are very little dispersion for some questions. Maybe they should be treated as factors instead of numeric
 
-SSPQ <- SSPQuestions
-Q <- rep('Q',111)
-num <- 1:111
-dat <- data.frame(Q,num)
-dat <- unite_(dat,'names', c("Q","num"), sep = '')
-names(SSPQ)[2:112] <-dat[,1] 
-#Removing all columns with only one value
-col <- rep(0,ncol(SSPQ)-1)
-for (i in 2:ncol(SSPQ)){
-  fac <- factor(SSPQ[,i])
-  if (length(levels(fac))==1){
-    col[i] <- i
-  }
-}
-SSPQ <- SSPQ[,-col]
 fit <- glm(Status~1,data = SSPQ,family = binomial(link='logit'))
 add <- add1(fit, scope = names(SSPQ)[2:108],test = 'Chisq')
 add[which.min(add$`Pr(>Chi)`),]
