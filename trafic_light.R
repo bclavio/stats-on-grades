4f579f2b5339c7700d72717e7a02784071ed39b9
nvihrs14 <nvihrs14@student.aau.dk>
2018-07-26 12:18:02 +0200

    making some more plots

--- trafic_light.R
+++ trafic_light.R
@@ -85,6 +85,13 @@ test$X6_Pr_nom <- test$X5_Pr_nom+test$X6_Pr_nom_insem
 
 MedDataBSc <- test[,-c(193:2013)]
 
+MedDataBSc$X1_all_atpIn <- rowSums(MedDataBSc[,c('X1_T_atpIn','X1_NT_atpIn','X1_Pr_atpIn','X1_El_atpIn')],na.rm = T)
+MedDataBSc$X2_all_atpIn <- rowSums(MedDataBSc[,c('X2_T_atpIn','X2_NT_atpIn','X2_Pr_atpIn','X2_El_atpIn')],na.rm = T)
+MedDataBSc$X3_all_atpIn <- rowSums(MedDataBSc[,c('X3_T_atpIn','X3_NT_atpIn','X3_Pr_atpIn','X3_El_atpIn')],na.rm = T)
+MedDataBSc$X4_all_atpIn <- rowSums(MedDataBSc[,c('X4_T_atpIn','X4_NT_atpIn','X4_Pr_atpIn','X4_El_atpIn')],na.rm = T)
+MedDataBSc$X5_all_atpIn <- rowSums(MedDataBSc[,c('X5_T_atpIn','X5_NT_atpIn','X5_Pr_atpIn','X5_El_atpIn')],na.rm = T)
+
+
 MedDataBSc$X1_all_atnBy <- rowSums(MedDataBSc[,c('X1_T_atnBy','X1_NT_atnBy','X1_Pr_atnBy','X1_El_atnBy')],na.rm = T)
 MedDataBSc$X2_all_atnBy <- rowSums(MedDataBSc[,c('X2_T_atnBy','X2_NT_atnBy','X2_Pr_atnBy','X2_El_atnBy')],na.rm = T)
 MedDataBSc$X3_all_atnBy <- rowSums(MedDataBSc[,c('X3_T_atnBy','X3_NT_atnBy','X3_Pr_atnBy','X3_El_atnBy')],na.rm = T)
@@ -149,48 +156,81 @@ MedDataBSc.allsem <- rbind(MedDataBSc.1,MedDataBSc.2,MedDataBSc.3,MedDataBSc.4,M
 MedDataBSc.allsem$startaar <- factor(MedDataBSc.allsem$startaar)
 MedDataBSc.allsem$semester <- factor(MedDataBSc.allsem$semester)
 
+#including only students who attempted something on previous semester
+MedDataBSc.allactive.1 <- MedDataBSc[MedDataBSc$EndSemester>1 & MedDataBSc$X1_all_atpIn>0,c('fradatosn','slutdatosn','statussn','EndSemester','startaar','Campus','dropout','X1_T_missing','X1_NT_missing','X1_Pr_missing','X1_all_missing','X1_TF')]
+names(MedDataBSc.allactive.1)[8:12] <- c('T_missing','NT_missing','Pr_missing','all_missing','TF')
+MedDataBSc.allactive.1$semester <- 1
+MedDataBSc.allactive.2 <- MedDataBSc[MedDataBSc$EndSemester>2& MedDataBSc$X2_all_atpIn>0,c('fradatosn','slutdatosn','statussn','EndSemester','startaar','Campus','dropout','X2_T_missing','X2_NT_missing','X2_Pr_missing','X2_all_missing','X2_TF')]
+names(MedDataBSc.allactive.2)[8:12] <- c('T_missing','NT_missing','Pr_missing','all_missing','TF')
+MedDataBSc.allactive.2$semester <- 2
+MedDataBSc.allactive.3 <- MedDataBSc[MedDataBSc$EndSemester>3& MedDataBSc$X3_all_atpIn>0,c('fradatosn','slutdatosn','statussn','EndSemester','startaar','Campus','dropout','X3_T_missing','X3_NT_missing','X3_Pr_missing','X3_all_missing','X3_TF')]
+names(MedDataBSc.allactive.3)[8:12] <- c('T_missing','NT_missing','Pr_missing','all_missing','TF')
+MedDataBSc.allactive.3$semester <- 3
+MedDataBSc.allactive.4 <- MedDataBSc[MedDataBSc$EndSemester>4& MedDataBSc$X4_all_atpIn>0,c('fradatosn','slutdatosn','statussn','EndSemester','startaar','Campus','dropout','X4_T_missing','X4_NT_missing','X4_Pr_missing','X4_all_missing','X4_TF')]
+names(MedDataBSc.allactive.4)[8:12] <- c('T_missing','NT_missing','Pr_missing','all_missing','TF')
+MedDataBSc.allactive.4$semester <- 4
+MedDataBSc.allactive.5 <- MedDataBSc[MedDataBSc$EndSemester>5& MedDataBSc$X5_all_atpIn>0,c('fradatosn','slutdatosn','statussn','EndSemester','startaar','Campus','dropout','X5_T_missing','X5_NT_missing','X5_Pr_missing','X5_all_missing','X5_TF')]
+names(MedDataBSc.allactive.5)[8:12] <- c('T_missing','NT_missing','Pr_missing','all_missing','TF')
+MedDataBSc.allactive.5$semester <- 5
+
+MedDataBSc.allactive.allsem <- rbind(MedDataBSc.allactive.1,MedDataBSc.allactive.2,MedDataBSc.allactive.3,MedDataBSc.allactive.4,MedDataBSc.allactive.5)
+MedDataBSc.allactive.allsem$startaar <- factor(MedDataBSc.allactive.allsem$startaar)
+MedDataBSc.allactive.allsem$semester <- factor(MedDataBSc.allactive.allsem$semester)
+
 #TF and dropout
+#all students who hasn't dropped out
 ggplot(data=MedDataBSc.allsem,aes(x=TF,fill=dropout))+
   geom_bar()+
-  facet_wrap(~semester,labeller = as_labeller(c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')))+
+  facet_wrap(~semester,labeller = as_labeller(c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')),scales = 'free_x')+
+  scale_fill_manual(values = c('forestgreen','darkred'),name='Status')+
+  ylab('Count')+
+  xlab('Trafic light category')+
+  theme(legend.position = c(0.85,0.25))
+
+ggplot(data=MedDataBSc.allsem,aes(x=TF,fill=dropout))+
+  geom_bar(position = 'fill')+
+  facet_wrap(~semester,labeller = as_labeller(c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')),scales = 'free_x')+
+  scale_fill_manual(values = c('forestgreen','darkred'),name='Status')+
+  ylab('Proportion')+
+  xlab('Trafic light category')+
+  theme(legend.position = c(0.85,0.25))  
+
+ggplot(data=MedDataBSc.allsem,aes(x=TF,fill=dropout))+
+  geom_bar()+
+  facet_grid(startaar~semester,labeller =labeller(semester=c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')),scales = 'free_x' )+
   scale_fill_manual(values = c('forestgreen','darkred'),name='Status')+
   ylab('Count')+
   xlab('Trafic light category')
 
 ggplot(data=MedDataBSc.allsem,aes(x=TF,fill=dropout))+
   geom_bar(position = 'fill')+
-  facet_wrap(~semester,labeller = as_labeller(c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')))+
+  facet_grid(startaar~semester,labeller = labeller(semester=c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')),scales = 'free_x')+
   scale_fill_manual(values = c('forestgreen','darkred'),name='Status')+
   ylab('Proportion')+
-  xlab('Trafic light category')  
+  xlab('Trafic light category')
 
 ggplot(data=MedDataBSc.allsem,aes(x=TF,fill=dropout))+
   geom_bar()+
-  facet_wrap(semester~startaar,labeller =labeller(semester=c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')) )+
+  facet_grid(Campus~semester,labeller =labeller(semester=c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')))+
   scale_fill_manual(values = c('forestgreen','darkred'),name='Status')+
   ylab('Count')+
   xlab('Trafic light category')
 
 ggplot(data=MedDataBSc.allsem,aes(x=TF,fill=dropout))+
   geom_bar(position = 'fill')+
-  facet_wrap(~semester+startaar,labeller = labeller(semester=c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')))+
+  facet_grid(Campus~semester,labeller =labeller(semester=c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')))+
   scale_fill_manual(values = c('forestgreen','darkred'),name='Status')+
-  ylab('Proportion')+
+  ylab('Count')+
   xlab('Trafic light category')
 
 ggplot(data=MedDataBSc.allsem,aes(x=dropout,fill=TF))+
   geom_bar(position = 'fill')+
-  facet_wrap(~semester,labeller = as_labeller(c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')))+
+  facet_wrap(~semester,labeller = as_labeller(c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')),scales = 'free_x')+
   scale_fill_manual(values = c('forestgreen','gold2','darkred'),name='Trafic light')+
   ylab('Proportion')+
-  xlab('Status')
+  xlab('Status')+
+  theme(legend.position = c(0.8,0.25))
 
-ggplot(data=MedDataBSc.allsem,aes(x=dropout,fill=TF))+
-  geom_bar(position = 'fill')+
-  facet_wrap(~semester+startaar,labeller = labeller(semester=c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')))+
-  scale_fill_manual(values = c('forestgreen','gold2','darkred'),name='Trafic light')+
-  ylab('Proportion')+
-  xlab('Status')
 
 ggplot(data=MedDataBSc.allsem,aes(x=semester,y=all_missing,col=dropout))+
   geom_boxplot(position = 'dodge',outlier.shape = NA)+
@@ -224,22 +264,100 @@ ggplot(data=MedDataBSc.allsem,aes(x=semester,y=Pr_missing,col=dropout))+
   ylab('Missing ECTS in projects')+
   xlab('Semester')
 
+#For students who actually attempted ECTS as well
+
+ggplot(data=MedDataBSc.allactive.allsem,aes(x=TF,fill=dropout))+
+  geom_bar()+
+  facet_wrap(~semester,labeller = as_labeller(c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')))+
+  scale_fill_manual(values = c('forestgreen','darkred'),name='Status')+
+  ylab('Count')+
+  xlab('Trafic light category')
+
+ggplot(data=MedDataBSc.allactive.allsem,aes(x=TF,fill=dropout))+
+  geom_bar(position = 'fill')+
+  facet_wrap(~semester,labeller = as_labeller(c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')))+
+  scale_fill_manual(values = c('forestgreen','darkred'),name='Status')+
+  ylab('Proportion')+
+  xlab('Trafic light category')  
+
+ggplot(data=MedDataBSc.allactive.allsem,aes(x=TF,fill=dropout))+
+  geom_bar()+
+  facet_wrap(semester~startaar,labeller =labeller(semester=c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')) )+
+  scale_fill_manual(values = c('forestgreen','darkred'),name='Status')+
+  ylab('Count')+
+  xlab('Trafic light category')
+
+ggplot(data=MedDataBSc.allactive.allsem,aes(x=TF,fill=dropout))+
+  geom_bar(position = 'fill')+
+  facet_wrap(~semester+startaar,labeller = labeller(semester=c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')))+
+  scale_fill_manual(values = c('forestgreen','darkred'),name='Status')+
+  ylab('Proportion')+
+  xlab('Trafic light category')
+
+ggplot(data=MedDataBSc.allactive.allsem,aes(x=dropout,fill=TF))+
+  geom_bar(position = 'fill')+
+  facet_wrap(~semester,labeller = as_labeller(c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')))+
+  scale_fill_manual(values = c('forestgreen','gold2','darkred'),name='Trafic light')+
+  ylab('Proportion')+
+  xlab('Status')
+
+ggplot(data=MedDataBSc.allactive.allsem,aes(x=dropout,fill=TF))+
+  geom_bar(position = 'fill')+
+  facet_wrap(~semester+startaar,labeller = labeller(semester=c('1'='After 1. semester','2'='After 2. semester','3'='After 3. semester','4'='After 4. semester','5'='After 5. semester')))+
+  scale_fill_manual(values = c('forestgreen','gold2','darkred'),name='Trafic light')+
+  ylab('Proportion')+
+  xlab('Status')
+
+ggplot(data=MedDataBSc.allactive.allsem,aes(x=semester,y=all_missing,col=dropout))+
+  geom_boxplot(position = 'dodge',outlier.shape = NA)+
+  geom_count(alpha=0.5,position = position_dodge(0.75))+
+  scale_colour_manual(values = c('forestgreen','darkred'),name='Status')+
+  scale_size_continuous(name='Count')+
+  ylab('Missing ECTS')+
+  xlab('Semester')
+
+ggplot(data=MedDataBSc.allactive.allsem,aes(x=semester,y=T_missing,col=dropout))+
+  geom_boxplot(position = 'dodge',outlier.shape = NA)+
+  geom_count(alpha=0.5,position = position_dodge(0.75))+
+  scale_colour_manual(values = c('forestgreen','darkred'),name='Status')+
+  scale_size_continuous(name='Count')+
+  ylab('Missing ECTS in technical courses')+
+  xlab('Semester')
+
+ggplot(data=MedDataBSc.allactive.allsem,aes(x=semester,y=NT_missing,col=dropout))+
+  geom_boxplot(position = 'dodge',outlier.shape = NA)+
+  geom_count(alpha=0.5,position = position_dodge(0.75))+
+  scale_colour_manual(values = c('forestgreen','darkred'),name='Status')+
+  scale_size_continuous(name='Count')+
+  ylab('Missing ECTS in non-technical courses')+
+  xlab('Semester')
+
+ggplot(data=MedDataBSc.allactive.allsem,aes(x=semester,y=Pr_missing,col=dropout))+
+  geom_boxplot(position = 'dodge',outlier.shape = NA)+
+  geom_count(alpha=0.5,position = position_dodge(0.75))+
+  scale_colour_manual(values = c('forestgreen','darkred'),name='Status')+
+  scale_size_continuous(name='Count')+
+  ylab('Missing ECTS in projects')+
+  xlab('Semester')
+
+
+#Classification trees to determine cutpoint
 tree.all.1 <- rpart(dropout~all_missing,data = MedDataBSc.1)
-rpart.plot(tree.all.1)
+rpart.plot(tree.all.1,main='After 1. semester')
 plotcp(tree.all.1)
 
 tree.all.2 <- rpart(dropout~all_missing,data = MedDataBSc.2)
 rpart.plot(tree.all.2)
 plotcp(tree.all.2)
 tree.all.2 <- rpart(dropout~all_missing,data = MedDataBSc.2,cp=0.084)
-rpart.plot(tree.all.2)
+rpart.plot(tree.all.2,main='After 2. semester')
 
 tree.all.3 <- rpart(dropout~all_missing,data = MedDataBSc.3)
-rpart.plot(tree.all.3)
+rpart.plot(tree.all.3,main='After 3. semester')
 plotcp(tree.all.3)
 
 tree.all.4 <- rpart(dropout~all_missing,data = MedDataBSc.4)
-rpart.plot(tree.all.4)
+rpart.plot(tree.all.4,main='After 4. semester')
 plotcp(tree.all.4)
 
 tree.all.5 <- rpart(dropout~all_missing,data = MedDataBSc.5)
@@ -252,28 +370,28 @@ rpart.plot(tree.category.1)
 tree.category.1$variable.importance
 plotcp(tree.category.1)
 tree.category.1 <- rpart(dropout~T_missing+NT_missing+Pr_missing,data = MedDataBSc.1,cp=0.12)
-rpart.plot(tree.category.1)
+rpart.plot(tree.category.1,main='After 1. semester')
 
 tree.category.2 <- rpart(dropout~T_missing+NT_missing+Pr_missing,data = MedDataBSc.2)
 rpart.plot(tree.category.2)
 tree.category.2$variable.importance
 plotcp(tree.category.2)
 tree.category.2 <- rpart(dropout~T_missing+NT_missing+Pr_missing,data = MedDataBSc.2,cp=0.084)
-rpart.plot(tree.category.2)
+rpart.plot(tree.category.2,main='After 2. semester')
 
 tree.category.3 <- rpart(dropout~T_missing+NT_missing+Pr_missing,data = MedDataBSc.3)
 rpart.plot(tree.category.3)
 tree.category.3$variable.importance
 plotcp(tree.category.3)
 tree.category.3 <- rpart(dropout~T_missing+NT_missing+Pr_missing,data = MedDataBSc.3,cp=0.091)
-rpart.plot(tree.category.3)
+rpart.plot(tree.category.3,main='After 3. semester')
 
 tree.category.4 <- rpart(dropout~T_missing+NT_missing+Pr_missing,data = MedDataBSc.4)
 rpart.plot(tree.category.4)
 tree.category.4$variable.importance
 plotcp(tree.category.4)
 tree.category.4 <- rpart(dropout~T_missing+NT_missing+Pr_missing,data = MedDataBSc.4,cp=0.17)
-rpart.plot(tree.category.4)
+rpart.plot(tree.category.4,main='After 4. semester')
 
 tree.category.5 <- rpart(dropout~T_missing+NT_missing+Pr_missing,data = MedDataBSc.5)
 rpart.plot(tree.category.5)
